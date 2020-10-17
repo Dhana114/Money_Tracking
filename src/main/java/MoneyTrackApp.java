69fc6146e83ba57fc1866ed5962953663ecf9035
dhana <ponugubatidhanalakshmi4444@gmail.com>
2020-10-18 00:24:12 +0200

    Implemented remove method, Added category ENUM

--- src/main/java/MoneyTrackApp.java
+++ src/main/java/MoneyTrackApp.java
@@ -1,19 +1,12 @@
-import models.ExpenseModel;
-import models.IncomeModel;
 import services.TransactionService;
 
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MoneyTrackApp {
-    enum expense
-    {
-        category, description, amount, month ;
-    }
-    enum income
-    {
-        title, amount, month ;
-    }
+
+
+
 
 
     public static void main(String[] args) throws IOException {
@@ -21,154 +14,41 @@ public class MoneyTrackApp {
         //System.out.println(expense.values()[0]);
         TransactionService transactionService = new TransactionService();
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
         System.out.println("Display/Add/edit");
+        System.out.println("Enter 1 to Display Income/Expense");
+        System.out.println("Enter 2 to Add Income/Expense");
+        System.out.println("Enter 3 to Edit Income/Expense");
+        System.out.println("Enter 4 to Remove Income/Expense");
         int inpType = myObj.nextInt();
-        if(inpType == 1){
 
-            System.out.println("Enter  Expense/income");
-            System.out.println("Enter 1 for expense");
-            System.out.println("Enter 2 for income");
+        if(inpType == 1) {
 
-            inpType = myObj.nextInt();
-            transactionService.readJsonFile(inpType==1?"expense":"income");
-
+            transactionService.displayDetails();
         }
         else if (inpType == 2) {
 
-
-            System.out.println("Enter  Expense/income");
-            System.out.println("Enter 1 for expense");
-            System.out.println("Enter 2 for income");
-
-            inpType = myObj.nextInt();
-
-            if (inpType == 1) {
-                System.out.println("Enter  Category");
-
-                String category = myObj.next();  // Reading category input
-
-                System.out.println("Enter  Descritpion");
-
-                String des = myObj.next();  // Reading title input
-
-                System.out.println("Enter amount ");
-
-                double amount = myObj.nextDouble(); // Reading amount input
-
-
-                System.out.println("Enter Month");
-
-                int month = myObj.nextInt(); // Reading month input
-
-
-                // Creating TransactionService object
-
-                // Adding input values to income model
-                ExpenseModel expenseModel = transactionService.addExpense(category, des, month, amount);
-
-
-                transactionService.saveJsonToFile(null, expenseModel, "expense");
-            }
-            if (inpType == 2) {
-                System.out.println("Enter  title");
-
-                String title = myObj.next();  // Reading title input
-
-                System.out.println("Enter amount ");
-
-                double amount = myObj.nextDouble(); // Reading amount input
-
-
-                System.out.println("Enter Month");
-
-                int month = myObj.nextInt(); // Reading month input
-
-
-                // Adding input values to income model
-                IncomeModel incomeModel = transactionService.addIncome(title, amount, month);
-
-
-                transactionService.saveJsonToFile(incomeModel, null, "income");
-
-
-            }
+            transactionService.addDetails();
         }
+
         else if(inpType == 3){
 
-            System.out.println("Enter  Expense/income");
-            System.out.println("Enter 1 for expense");
-            System.out.println("Enter 2 for income");
+            transactionService.editDetails();
 
-            int choice = myObj.nextInt();
+        }
+        else if(inpType == 4){
 
-            if(choice == 1) {
-
-                transactionService.readJsonFile("expense");
-                System.out.println("Please enter id you want to edit");
-                int id = myObj.nextInt();
-
-                System.out.println("Enter 1 to edit category");
-                System.out.println("Enter 2 to edit description");
-                System.out.println("Enter 3 to edit amount");
-                System.out.println("Enter 4 to edit month");
-
-                int key = myObj.nextInt();
-
-                System.out.println("Enter new value for selected" + expense.values()[key - 1]);
-
-                String value = myObj.next();
-
-                transactionService.editEntry(id, "expense", expense.values()[key - 1].toString(), value);
-                transactionService.readJsonFile("expense");
-            }
-                else if(choice == 2) {
-
-                    transactionService.readJsonFile("income");
-                    System.out.println("Please enter id you want to edit");
-                    int id = myObj.nextInt();
-
-                    System.out.println("Enter 1 to edit title");
-                    System.out.println("Enter 2 to edit amount");
-                    System.out.println("Enter 3 to edit month");
-
-                    int key = myObj.nextInt();
-
-                    System.out.println("Enter new value for selected" + income.values()[key - 1]);
-
-                    String value = myObj.next();
-
-                    transactionService.editEntry(id, "income", income.values()[key - 1].toString(), value);
-                    transactionService.readJsonFile("income");
-
-
-                }
-
-
-
-
-
-
-
-
-
-
-
+            transactionService.delete();
 
         }
 
 
 
-
-}
-
-
-
-
-
-
-
-
+        else{
+            System.out.println("Enter a valid number");
+        }
     }
+}
 
 
 
--- src/main/java/services/TransactionService.java
+++ src/main/java/services/TransactionService.java
@@ -8,8 +8,22 @@ import models.ExpenseModel;
 import models.IncomeModel;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class TransactionService {
+    enum expense
+    {
+        category, description, amount, month ;
+    }
+    enum income
+    {
+        title, amount, month ;
+    }
+    enum categories{
+
+        Food, Grocery, Shopping, Vacation, Household, BillsPayment;
+
+    }
 
     public IncomeModel addIncome(String title, double amount, int month){
 
@@ -166,7 +180,7 @@ public class TransactionService {
 
 
 
-public void editEntry(int id, String choice, String key, String value) throws IOException {
+public void editJsonEntry(int id, String choice, String key, String value) throws IOException {
 
     // Reading json file
     FileReader jsonFile = new FileReader("/Users/dhanalakshmi/Downloads/income.json");
@@ -184,17 +198,17 @@ public void editEntry(int id, String choice, String key, String value) throws IO
     JsonArray choiceJson = (JsonArray) jsonFileObject.get(choice);
     double amount = 0.0;
     int month = 0;
-if (key.equals("amount")){
+    if (key.equals("amount")){
     amount  = Double.parseDouble(value);
     choiceJson.get(id - 1).getAsJsonObject().addProperty(key,amount);
-}
-else if(key.equals("month")){
+    }
+    else if(key.equals("month")){
     month = Integer.parseInt(value);
     choiceJson.get(id - 1).getAsJsonObject().addProperty(key,month);
-}
-else {
+    }
+    else {
     choiceJson.get(id - 1).getAsJsonObject().addProperty(key, value);
-}
+    }
 
     try{
 
@@ -215,7 +229,275 @@ else {
 
 
 }
+
+    public void editDetails() throws IOException {
+
+        TransactionService transactionService = new TransactionService();
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+      //  int inpType = myObj.nextInt();
+        System.out.println("Enter  Expense/income");
+        System.out.println("Enter 1 for expense");
+        System.out.println("Enter 2 for income");
+
+        int choice = myObj.nextInt();
+
+        if (choice == 1) {
+
+            transactionService.readJsonFile("expense");
+            System.out.println("Please enter id you want to edit");
+            int id = myObj.nextInt();
+
+            System.out.println("Enter 1 to edit category");
+            System.out.println("Enter 2 to edit description");
+            System.out.println("Enter 3 to edit amount");
+            System.out.println("Enter 4 to edit month");
+
+            int key = myObj.nextInt();
+
+            if (key == 1){
+
+                System.out.println("Change category from the below list");
+                for (categories c : categories.values()) {
+                    System.out.println(  c);
+                }
+            }
+
+            System.out.println("Enter new value for selected" + TransactionService.expense.values()[key - 1]);
+
+            String value = myObj.next();
+
+            transactionService.editJsonEntry(id, "expense", TransactionService.expense.values()[key - 1].toString(), value);
+            transactionService.readJsonFile("expense");
+        }
+        else if (choice == 2) {
+
+            transactionService.readJsonFile("income");
+            System.out.println("Please enter id you want to edit");
+            int id = myObj.nextInt();
+
+            System.out.println("Enter 1 to edit title");
+            System.out.println("Enter 2 to edit amount");
+            System.out.println("Enter 3 to edit month");
+
+            int key = myObj.nextInt();
+
+            System.out.println("Enter new value for selected" + TransactionService.income.values()[key - 1]);
+
+            String value = myObj.next();
+
+            transactionService.editJsonEntry(id, "income", TransactionService.income.values()[key - 1].toString(), value);
+            transactionService.readJsonFile("income");
+
+
+        }
+
+        else{
+
+            System.out.println("Enter a Valid input");
+            transactionService.editDetails();
+
+        }
     }
+
+    public void addDetails() throws FileNotFoundException {
+        TransactionService transactionService = new TransactionService();
+
+        System.out.println("Enter  Expense/income");
+        System.out.println("Enter 1 for expense");
+        System.out.println("Enter 2 for income");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        int inpType = myObj.nextInt();
+        if (inpType == 1) {
+            System.out.println("Choose Category from the list");
+
+            int i = 1;
+            while (i <= 5) {
+                for (categories c : categories.values()) {
+                    System.out.println(i + "." + c);
+                    i++;
+                }
+
+            }
+
+
+            String cat = myObj.next();  // Reading category input
+
+           /*// for (int c = myObj.nextInt(); c < category.values().length; c++)
+            categories c;
+                for ( c : categories.values()) {
+                    if(cat == (c.ordinal())+1){
+
+                        System.out.println("The category you choose is" + c);
+
+                    }
+                }*/
+
+
+
+
+            System.out.println("Enter  Descritpion");
+
+            String des = myObj.next();  // Reading title input
+
+            System.out.println("Enter amount ");
+
+            double amount = myObj.nextDouble(); // Reading amount input
+
+
+            System.out.println("Enter Month");
+
+            int month = myObj.nextInt(); // Reading month input
+
+
+            // Creating TransactionService object
+
+            // Adding input values to income model
+            ExpenseModel expenseModel = transactionService.addExpense(cat, des, month, amount);
+
+
+            transactionService.saveJsonToFile(null, expenseModel, "expense");
+
+        }
+        else if (inpType == 2) {
+            System.out.println("Enter  title");
+
+            String title = myObj.next();  // Reading title input
+
+            System.out.println("Enter amount ");
+
+            double amount = myObj.nextDouble(); // Reading amount input
+
+
+            System.out.println("Enter Month");
+
+            int month = myObj.nextInt(); // Reading month input
+
+
+            // Adding input values to income model
+            IncomeModel incomeModel = transactionService.addIncome(title, amount, month);
+
+
+            transactionService.saveJsonToFile(incomeModel, null, "income");
+
+
+        }
+
+        else{
+
+            System.out.println("Enter a Valid input");
+            transactionService.addDetails();
+
+        }
+    }
+
+    public void displayDetails(){
+        TransactionService transactionService = new TransactionService();
+
+
+
+        System.out.println("Enter  Expense/income");
+        System.out.println("Enter 1 for expense");
+        System.out.println("Enter 2 for income");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        int inpType = myObj.nextInt();
+        transactionService.readJsonFile(inpType==1?"expense":"income");
+
+    }
+
+    public void removeEntry(String choice) throws FileNotFoundException {
+
+        TransactionService transactionService = new TransactionService();
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        int inpType = myObj.nextInt();
+        FileReader jsonFile = new FileReader("/Users/dhanalakshmi/Downloads/income.json");
+        JsonParser parser = new JsonParser();
+
+
+
+        JsonObject jsonFileObject = new JsonObject();
+
+        // Retrieving  json file data as json object
+        jsonFileObject =  parser.parse(jsonFile).getAsJsonObject();
+        // System.out.println("json object is:" + jsonFileObject);
+        // get income value from json object if not null
+
+        JsonArray choiceJson = (JsonArray) jsonFileObject.get(choice);
+
+        if (choice.equals("expense")) {
+
+            transactionService.readJsonFile("expense");
+            System.out.println("Please enter id you want to remove");
+            int id = myObj.nextInt();
+            if (id > 0 && id < choiceJson.size()-1){
+                choiceJson.remove(id -1);
+            }
+
+            else {
+
+                System.out.println("Enter a valid input");
+                transactionService.removeEntry(choice);
+            }
+
+            System.out.println("Succesfully deleted the expense at id " + id);
+        }
+
+        else {
+
+            transactionService.readJsonFile("income");
+            System.out.println("Please enter id you want to remove");
+            int id = myObj.nextInt();
+            if (id > 0 && id < choiceJson.size()-1){
+                choiceJson.remove(id - 1);
+            }
+
+            else {
+
+                System.out.println("Enter a valid input");
+                transactionService.removeEntry(choice);
+            }
+
+            System.out.println("Succesfully deleted the income at id " + id);
+        }
+        try{
+
+//          File Writer creates a file in write mode at the given location
+
+            FileWriter file = new FileWriter("/Users/dhanalakshmi/Downloads/income.json");
+
+//          Here we convert the obj data to string and put/write it inside the json file
+
+            file.write(jsonFileObject.toString());
+            file.flush();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+    public void delete() throws FileNotFoundException {
+
+        TransactionService transactionService = new TransactionService();
+
+
+
+        System.out.println("Enter  Expense/income");
+        System.out.println("Enter 1 for expense");
+        System.out.println("Enter 2 for income");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        int inpType = myObj.nextInt();
+        transactionService.removeEntry(inpType==1?"expense":"income");
+
+
+    }
+
+    }
+
+
 
 
 
